e88e93cdb23980e0ec2efa7cd474fc5766863a6a
unknown <jake.watkins@gmail.com>
2024-03-18 12:22:05 -0500

    update for az integration

--- demo-terraform/variables.tf
+++ demo-terraform/variables.tf
@@ -1,9 +1,3 @@
 
 variable "resource_location" {}
 variable "resource_group_name" {}
-
-# for service principal
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
